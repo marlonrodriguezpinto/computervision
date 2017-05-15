2c871c43913215878e7b800dfa0e9e0894aa7ef2
Philipp Weiler <philippweiler@philipps-mbp-2.localdomain>
2017-05-15 21:46:37 +0200

    sobel filter version by philipp debugged and working

--- HA1/CVHA1.m
+++ HA1/CVHA1.m
@@ -5,16 +5,14 @@
 %  Einlesen und Konvertieren von Bildern sowie Bestimmung von 
 %  Merkmalen mittels Harris-Detektor. 
 
-%  Für die letztendliche Abgabe bitte die Kommentare in den folgenden Zeilen
-%  enfernen und sicherstellen, dass alle optionalen Parameter über den
-%  entsprechenden Funktionsaufruf fun('var',value) modifiziert werden können.
+%  F?r die letztendliche Abgabe bitte die Kommentare in den folgenden Zeilen
+%  enfernen und sicherstellen, dass alle optionalen Parameter ?ber den
+%  entsprechenden Funktionsaufruf fun('var',value) modifiziert werden k?nnen.
 
 
 %% Bild laden
 Image = imread('szene.jpg');
 IGray = rgb_to_gray(Image);
-
-
 
 
 %% Harris-Merkmale berechnen
